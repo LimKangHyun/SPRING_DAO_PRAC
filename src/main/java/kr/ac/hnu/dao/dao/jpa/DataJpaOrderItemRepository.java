ffd29017e5065925780cae27d75c30ead2933331
kanghyun <limkh06910691@gmail.com>
2026-06-04 13:23:57 +0900

    Add method to find OrderItems by orderCode
    Added a query method to find OrderItems by orderCode.

--- src/main/java/kr/ac/hnu/dao/dao/jpa/DataJpaOrderItemRepository.java
+++ src/main/java/kr/ac/hnu/dao/dao/jpa/DataJpaOrderItemRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public interface DataJpaOrderItemRepository extends JpaRepository<OrderItems, Long> {
 
+    // PK가 아닌 UNIQUE키로 데이터를 조회하고 싶을때, 쿼리 메서드 사용
     @Query("select oi from OrderItems oi where oi.orders.orderCode = :orderCode")
     List<OrderItems> findAllByOrderCode(String orderCode);
 }
