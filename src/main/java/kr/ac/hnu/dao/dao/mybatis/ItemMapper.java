832bebac4449e9fecc7079dabbfeb514b11c76bf
kanghyun <limkh06910691@gmail.com>
2026-07-03 13:44:25 +0900

    Clean up ItemMapper interface
    Removed commented code and adjusted formatting.

--- src/main/java/kr/ac/hnu/dao/dao/mybatis/ItemMapper.java
+++ src/main/java/kr/ac/hnu/dao/dao/mybatis/ItemMapper.java
@@ -6,12 +6,10 @@ import org.apache.ibatis.annotations.Param;
 
 import java.util.Optional;
 
-// @Mapper를 사용하면 myBatis가 자동으로 구현체로 인식
 @Mapper
 public interface ItemMapper {
     void save(Items items);
     int update(@Param("itemCode") String itemCode, @Param("price") Integer price);
     Optional<Items> findByItemCode(@Param("itemCode") String itemCode);
     void delete(Items items);
-    List<Items> findAll();
 }
